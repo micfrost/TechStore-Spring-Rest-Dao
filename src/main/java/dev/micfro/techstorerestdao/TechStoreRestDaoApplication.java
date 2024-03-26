d75ed29fc706f06c0b048171ec114f7a3e698457
micfro <michalfrost71@gmail.com>
2024-03-26 12:24:01 +0100

    Initial commit

---
+++ src/main/java/dev/micfro/techstorerestdao/TechStoreRestDaoApplication.java
@@ -0,0 +1,13 @@
+package dev.micfro.techstorerestdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechStoreRestDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechStoreRestDaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/micfro/techstorerestdao/TechStoreRestDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.micfro.techstorerestdao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechStoreRestDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
